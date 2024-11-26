87403181c7bca69aa928c1ffc09fdf466e2f7a51
maximka1366 <maximka66@mail.ru>
2024-11-26 21:34:43 +0300

    Add files via upload

---
+++ module_16_2.py
@@ -0,0 +1,27 @@
+from fastapi import FastAPI, Path
+from typing import Annotated
+
+
+app = FastAPI()
+
+
+@app.get('/')
+async def get_main_page() -> dict:
+    return {'message': "Главная страница"}
+
+@app.get('/user/admin')
+async def get_admin_page() -> dict:
+    return {'message': "Вы вошли как администратор"}
+
+@app.get("/user/{user_id}")
+async def get_user_number(user_id: Annotated[int, Path(ge=1, le=100, description="Enter User ID", example='99')]) -> dict:
+    return {"message": f"Вы вошли как пользователь № {user_id}"}
+
+
+@app.get("/user/{username}/{age}")
+async def user_info(username: Annotated[str, Path(min_length=5, max_length=20, description="Enter username", example='Maxim')],
+                    age: Annotated[int, Path(ge=18, le=120, description="Enter age", example='99')]) -> dict:
+    return {"message": f"Информация о пользователе. Имя: {username}, Возраст: {age}"}
+
+
+
